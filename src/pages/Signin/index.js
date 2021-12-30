59e0bd68a9c76055560395ce14dde949cc0473c4
Jhon9191 <joaoantonio15161516@gmail.com>
2021-12-30 18:54:42 -0300

    Configuring onChange in input components

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import './signin.css';
 import IputText from '../../Components/InputText';
 import InputTextSecure from '../../Components/InputTextSecure';
-import Text from '../../Components/Text';
 
 function SignIn() {
 
@@ -15,12 +14,17 @@ function SignIn() {
     setSecure(!secure);
   }
 
+  const handleValidation = () => {
+    console.log(email, password);
+  }
+
   return (
     <div className="container-center">
       <div className="login">
-      <IputText placeholder="E-mail"/>
-      <InputTextSecure placeholder="password" hasIcon={true} funcao={retorna} secure={secure}/>
-      
+      <h2>Entrar</h2>
+      <IputText placeholder="E-mail" funcao={(e) => setEmail(e.target.value)}/>
+      <InputTextSecure placeholder="password" onChange={(e) => setPassword(e.target.value)} hasIcon={true} funcao={retorna} secure={secure}/>
+      <button onClick={handleValidation}>Login</button>
       </div>
     </div>
   );
