3797c251ccd05c1e51072a4f9d154c4dd8d41997
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-02 21:49:16 -0300

    Creating Button component

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import './signin.css';
+
 import IputText from '../../Components/InputText';
 import InputTextSecure from '../../Components/InputTextSecure';
 import Button from '../../Components/Button';
@@ -9,6 +11,7 @@ function SignIn() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [secretKey, setSecretKey] = useState('');
   const [secure,setSecure] = useState(true);
 
   const retorna = () => {
@@ -22,10 +25,12 @@ function SignIn() {
   return (
     <div className="container-center">
       <div className="login">
-      <h2>Entrar</h2>
+      <h2>Já tem cadastro?</h2>
       <IputText placeholder="E-mail" funcao={(e) => setEmail(e.target.value)}/>
+      <IputText placeholder="Secret key" funcao={(e) => setSecretKey(e.target.value)}/>
       <InputTextSecure placeholder="Password" onChange={(e) => setPassword(e.target.value)} hasIcon={true} funcao={retorna} secure={secure}/>
       <Button name="Entrar"/>
+      <Link to="/register">Criar uma nova conta</Link>
       </div>
     </div>
   );
